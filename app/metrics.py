f58299bc05dbc2d0f58d2129800727ca70e819e9
Hector Luis Alamo <halamo24@gmail.com>
2025-11-04 17:12:23 -0800

    obs: Prometheus add-on in metrics.py

--- app/metrics.py
+++ app/metrics.py
@@ -1,6 +1,14 @@
 import threading
 from collections import defaultdict, deque
 
+try:
+    from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+    PROM = True
+    P_COUNT = Counter('analyze_requests_total', 'Total analyze requests')
+    H_LAT = Histogram('analyze_latency_ms', 'Analyze latency ms', buckets=(100, 250, 500, 1000, 2000, 5000))
+except Exception:
+    PROM = False
+
 _lock = threading.Lock()
 counters = defaultdict(int)
 timings_ms = defaultdict(list, lambda: deque(maxlen=5000))
